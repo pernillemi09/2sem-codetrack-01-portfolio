4d9e60d7bbc2e44f05dfc3fb2a66b2935c31bea4
Martin Dilling-Hansen <madh@zealand.dk>
2025-08-06 17:16:27 +0200

    Added favicon

--- views/layout.view.php
+++ views/layout.view.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php $this->section('title') ?> - My Portfolio</title>
+    <link rel="icon" href="favicon.png" type="image/png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Merriweather:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
